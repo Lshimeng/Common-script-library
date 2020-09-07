2497325821fd8a33bb9dab47b9837224e244c1a7
LIU <66361302+Lshimeng@users.noreply.github.com>
2020-09-07 16:14:36 +0800

    Add files via upload

---
+++ Batch change suffix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#coding:utf8
+#! python3
+#批量修改一个文件下的文件后缀
+import sys
+import os
+def Rename():
+  Path = r"c:\Users\Liush\Desktop\TJTG\12\model";
+  filelist = os.listdir(Path)
+  for files in filelist:
+    Olddir = os.path.join(Path,files)
+    print(files)  #打印出老的文件夹里的目录和文件
+    if os.path.isdir(Olddir):  #判断是否是文件，是文件，跳过
+      continue
+    filename = os.path.splitext(files)[0]
+    #filetype = os.path.splitext(files)[1]
+    Newdir = os.path.join(Path,filename + '.xls')  #只要修改后缀名就可以更改成任意想要的格式
+    os.rename(Olddir,Newdir)
+Rename()
